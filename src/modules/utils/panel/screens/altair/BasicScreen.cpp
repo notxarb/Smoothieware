5d1fc097fe26701f1d67a621a1d7b1e908745400
Allen <hal09014@gmail.com>
2016-09-27 18:53:01 -0600

    Hey, Here is your stuff

--- src/modules/utils/panel/screens/altair/BasicScreen.cpp
+++ src/modules/utils/panel/screens/altair/BasicScreen.cpp
@@ -1,68 +1,88 @@
 /*
-      This file is part of Smoothie (http://smoothieware.org/). The motion control part is heavily based on Grbl (https://github.com/simen/grbl).
-      Smoothie is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-      Smoothie is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-      You should have received a copy of the GNU General Public License along with Smoothie. If not, see <http://www.gnu.org/licenses/>.
+This file is part of Smoothie (http://smoothieware.org/). The motion control part is heavily based on Grbl (https://github.com/simen/grbl).
+Smoothie is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Smoothie is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Smoothie. If not, see <http://www.gnu.org/licenses/>.
 */
-
-#include "libs/Kernel.h"
-#include "Panel.h"
-#include "PanelScreen.h"
-#include "LcdBase.h"
 #include "BasicScreen.h"
-#include "WatchScreen.h"
-#include "FileScreen.h"
-#include "JogScreen.h"
-#include "ControlScreen.h"
-#include "PrepareScreen.h"
-#include "ProbeScreen.h"
-#include "libs/nuts_bolts.h"
-#include "libs/utils.h"
-#include "modules/utils/player/PlayerPublicAccess.h"
-#include "modules/utils/player/Player.h"
-#include "PublicData.h"
+#include "libs/Kernel.h"
+#include "Config.h"
 #include "checksumm.h"
-#include "ModifyValuesScreen.h"
-#include "Robot.h"
-#include "Planner.h"
-#include "StepperMotor.h"
-#include "EndstopsPublicAccess.h"
+#include "LcdBase.h"
+#include "Panel.h"
+#include "ConfigValue.h"
 
-#include <string>
+#include <string.h>
+
+#include <algorithm>
+
+#define enable_checksum      CHECKSUM("enable")
+#define custom_menu_checksum CHECKSUM("custom_menu")
+#define name_checksum        CHECKSUM("name")
+#define command_checksum     CHECKSUM("command")
+
 using namespace std;
 
 BasicScreen::BasicScreen()
 {
-    // Children screens
+	//printf("Setting up CustomScreen\n");
+	vector<uint16_t> modules;
+	THEKERNEL->config->get_module_list(&modules, custom_menu_checksum);
+
+	// load the custom menu items
+	for (unsigned int i = 0; i < modules.size(); i++) {
+		if (THEKERNEL->config->value(custom_menu_checksum, modules[i], enable_checksum)->as_bool()) {
+			// Get Menu entry name
+			string name = THEKERNEL->config->value(custom_menu_checksum, modules[i], name_checksum)->as_string();
+			std::replace(name.begin(), name.end(), '_', ' '); // replace _ with space
+
+			// Get Command
+			string command = THEKERNEL->config->value(custom_menu_checksum, modules[i], command_checksum)->as_string();
+			std::replace(command.begin(), command.end(), '_', ' '); // replace _ with space
+			std::replace(command.begin(), command.end(), '|', '\n'); // replace | with \n for multiple commands
+
+			// put in menu item list
+			menu_items.push_back(make_tuple(strdup(name.c_str()), strdup(command.c_str())));
+			//printf("added menu %s, command %s\n", name.c_str(), command.c_str());
+		}
+	}
 }
 
 void BasicScreen::on_enter()
 {
-    THEPANEL->enter_menu_mode();
-    THEPANEL->setup_menu(1);
-    this->refresh_menu();
+	THEPANEL->enter_menu_mode();
+	THEPANEL->setup_menu(menu_items.size() + 1);
+	this->refresh_menu();
 }
 
 void BasicScreen::on_refresh()
 {
-    if ( THEPANEL->menu_change() ) {
-        this->refresh_menu();
-    }
-    if ( THEPANEL->click() ) {
-        this->clicked_menu_entry(THEPANEL->get_menu_current_line());
-    }
+	if (THEPANEL->menu_change()) {
+		this->refresh_menu();
+	}
+	if (THEPANEL->click()) {
+		this->clicked_menu_entry(THEPANEL->get_menu_current_line());
+	}
 }
 
 void BasicScreen::display_menu_line(uint16_t line)
 {
-    switch(line) {
-        case 1: THEPANEL->lcd->printf("Back"); break;
-    }
+	if (line == 0) {
+		THEPANEL->lcd->printf("Back");
+	}
+	else {
+		THEPANEL->lcd->printf(std::get<0>(menu_items[line - 1]));
+	}
 }
 
 void BasicScreen::clicked_menu_entry(uint16_t line)
 {
-    switch(line) {
-        case 1: THEPANEL->enter_screen(this->parent); break;
-    }
+	if (line == 0) {
+		THEPANEL->enter_screen(this->parent);
+	}
+	else {
+		send_command(std::get<1>(menu_items[line - 1])); // will be done in main loop
+		THEPANEL->enter_screen(this->parent);
+		// Returns to previous screen to show that a command was selected.
+	}
 }
